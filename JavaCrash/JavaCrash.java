6bb8424a5bb6080e990daa4cbcaa133823e663bd
cocoa <77078601+hotcocoadotcom@users.noreply.github.com>
2026-09-10 13:08:06 +0000

    commission

--- JavaCrash/JavaCrash.java
+++ JavaCrash/JavaCrash.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class JavaCrash {
 
     public static void main(String[] args) {
@@ -82,8 +84,72 @@ public class JavaCrash {
         int[] arry2 = {34, 52, 3, 64, 32};
 
         int[][] arryGrid1 = new int[4][3];
+        
+        int[][] arryGrid2 = { {7, 8, 9}, {4, 5, 6}, {1, 2, 3} };
 
+        System.out.println(arryGrid2[2][2] + arryGrid2[0][0]);
 
+        //ARRAYLISTS
+        //thehy can be any size, A LOT OF BUILT IN FUNCTION!
+
+        ArrayList<String> arryLst = new ArrayList<>();
+
+        arryLst.add("word one");
+        arryLst.add("word three");
+        arryLst.add("word seventeen");
+
+        arryLst.remove(0);
+
+        arryLst.add(0, "word feivjreag");
+
+        arryLst.set(1, "word JGFIOWERHJG");
+
+        System.out.println(arryLst);
+        System.out.println(arryLst.size());
+        System.out.println(arryLst.get(2));
+
+        // Math Mathematicals
+
+        System.out.println(Math.max(13, 9));
+        System.out.println(Math.min(13, 9));
+        System.out.println(Math.sqrt(16));
+        System.out.println(Math.abs(-5));
+
+        if(f) {
+            System.out.println("hello");
+        } else if (!f) {
+
+        } else {
+            System.out.println("else");
+        }
+
+        int x = 5;
+        while (x < 10) {
+            System.out.println(x);
+            x++;
+        }
+
+        for(int i=0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+        //ENHANCED!!! for loop
+
+        ArrayList<String> animals = new ArrayList<>();
+
+        animals.add("Sheep");
+        animals.add("Deer");
+        animals.add("Meese");
+
+        System.out.println(animals);
+
+        for (String s : animals) {
+            System.out.println(s);
+        }
+
+        for (int i=0; i < arry2.length; i++) {
+            System.out.println(arry2[4]);
+        }
     }
 
 }
---
+++ Oople.java
@@ -0,0 +1,16 @@
+public class Oople {
+    
+    public static void main(String[] args) {
+
+        // attributes / data
+        // variables
+
+        // constructor --- special method called every time you make an object
+        // same name as the class followed by parenthesis
+        // Oople()
+
+        // behaviors / methods
+        // you can write as many as you vwant
+
+    }
+}
